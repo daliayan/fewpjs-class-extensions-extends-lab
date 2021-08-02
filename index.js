e7c974be705e2ed48ee4b875c5de7167fd3dd54b
daliayan <daliayan5@gmail.com>
2021-08-02 19:23:42 -0400

    Creating classes that inherit from other classes + methods for finding info

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+
+class Polygon{
+    constructor(sides){
+        this.sides = sides;
+        this.count = this.sides.length
+    }
+
+    get countSides(){
+        return this.sides.length 
+    }
+
+    get perimeter() {
+        if (!Array.isArray(this.sides)) return;
+        let sum = 0;
+        for (var int of this.sides) {
+          sum += int
+        }
+        return sum
+    }
+}
+
+class Triangle extends Polygon {
+
+    get isValid() {
+
+    }
+
+}
+
+class Square extends Polygon {
+    
+    get isValid() {
+
+    }
+
+    get area() {
+        if (!Array.isArray(this.sides)) return;
+        if (this.count !== 4) return;
+        return this.sides[0] * this.sides[0]
+    }
+}
